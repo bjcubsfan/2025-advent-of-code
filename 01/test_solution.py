0fc9b87ad81d84cf1b3fa9b6cedc0aafe2ad0136
B. J. Potter <bj.ctr.potter@faa.gov>
2025-12-01 15:36:53 -0600

    Still works!?!?

--- 01/test_solution.py
+++ 01/test_solution.py
@@ -15,19 +15,31 @@ R14
 L82
 """
 
-# @pytest.mark.parametrize(
-#    "encrypted_room, is_real_room",
-#    [
-#        ("aaaaa-bbb-z-y-x-123[abxyz]", True),
-#        ("a-b-c-d-e-f-g-h-987[abcde]", True),
-#        ("not-a-real-room-404[oarel]", True),
-#        ("totally-real-room-200[decoy]", False),
-#    ],)
 def test_part_1():
     calc_part_1 = part_1(input_data)
     assert calc_part_1 == 3
 
 
-def test_part_2():
-    calc_part_2 = part_2(input_data)
-    assert calc_part_2 == 6
+@pytest.mark.parametrize(
+   "this_input_data, expected",
+   [
+       ("""L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+""", 6),
+       ("""L500
+R1000
+L50
+L5
+""", 16),
+   ],)
+def test_part_2(this_input_data, expected):
+    calc_part_2 = part_2(this_input_data)
+    assert calc_part_2 == expected
