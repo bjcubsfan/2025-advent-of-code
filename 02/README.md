7ec4a81eb9da1ebb791efc642f769b2dff023238
B. J. Potter <bj.ctr.potter@faa.gov>
2025-12-02 15:26:04 -0600

    Finish Day 02 part 1

---
+++ 02/README.md
@@ -0,0 +1,11 @@
+# Day 2: Gift Shop
+
+https://adventofcode.com/2025/day/2
+
+## Part 1
+
+I finished in 23:39.
+
+## Part 2
+
+
--- 02/solution.py
+++ 02/solution.py
@@ -13,12 +13,41 @@ import pprint
 
 import docopt
 
+def is_invalid(id_to_check):
+    logging.debug(f"{id_to_check=}")
+    str_id = str(id_to_check)
+    if len(str_id) % 2 != 0:
+        return False
+    half_id = int(len(str_id) / 2)
+    first = str_id[:half_id]
+    last = str_id[half_id:]
+    if first == last:
+        logging.debug(f"FOUND ONE: {str_id}")
+        return True
+    else:
+        return False
+
+def sum_invalid_ids(id_range):
+    first_id, last_id = id_range.split("-")
+    first_id = int(first_id)
+    last_id = int(last_id)
+    this_range_sum = 0
+    for id_to_check in range(first_id, last_id + 1):
+        if is_invalid(id_to_check):
+            logging.debug("Adding it")
+            this_range_sum += id_to_check
+        else:
+            continue
+    return this_range_sum
 
 def part_1(input_data):
     input_data = input_data.strip()
-    answer = None
+    answer = 0
     for line in input_data.split("\n"):
         line = line.strip()
+        for id_range in line.split(","):
+            answer += sum_invalid_ids(id_range)
+            logging.debug(f"{answer=}")
     return answer
 
 
